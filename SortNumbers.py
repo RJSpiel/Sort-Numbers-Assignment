5ac3f9ca51e728591fb6defab3e8d84202cb5c83
Raymond Spieldenner <89670786+RJSpiel@users.noreply.github.com>
2022-02-01 10:36:13 -0800

    Number sorting program.
    This is a program I wrote for a python course I'm currently taking. There is a slight issue with the code where when inputting the largest number into the second number input, the program no longer displays what the middle number is.
    
    I don't know if anyone will ever see this or this repo in general, but if you do, feedback is always appreciated!

---
+++ SortNumbers.py
@@ -0,0 +1,32 @@
+#Name: Raymond J. Spieldenner
+#Assignment: If Else - Project: Sorting Numbers
+#Date: 02/01/2022
+
+#Input Variables:
+
+userInput1 = int(input('Please input first number: '))
+userInput2 = int(input('Please input second number: '))
+userInput3 = int(input('Please input third number: '))
+
+#Finding the largest number:
+if userInput1 > userInput2 and userInput1 > userInput3:
+    print(userInput1)
+elif userInput2 > userInput1 and userInput2 > userInput3:
+    print(userInput2)
+elif userInput3 > userInput1 and userInput3 > userInput2:
+    print(userInput3)
+#Finding the middle number:
+if userInput1 > userInput2 and userInput1 < userInput3:
+    print(userInput1)
+elif userInput2 > userInput1 and userInput2 < userInput3:
+    print(userInput2)
+elif userInput3 > userInput1 and userInput3 < userInput2:
+    print(userInput3)
+
+#finding the smallest number:
+if userInput1 < userInput2 and userInput1 < userInput3:
+    print(userInput1)
+elif userInput2 < userInput1 and userInput2 < userInput3:
+    print(userInput2)
+elif userInput3 < userInput1 and userInput3 < userInput2:
+    print(userInput3)
