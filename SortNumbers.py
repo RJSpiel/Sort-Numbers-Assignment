61a90b43eccff1dbe5eaff37b60e9de96b2ad9f3
Raymond Spieldenner <89670786+RJSpiel@users.noreply.github.com>
2022-02-02 15:31:01 -0800

    Bug fix - SortNumbers.py
    I ask a buddy of mine for some help with my input issue (thanks again Fabian!!), and after a little bit more testing we found what the issue was. When the userInput3 was less than both userInput1 and userInput2, the program would skip right over the middle section of code and only output the largest and the smallest values. He did a little more research and testing on his end, and we came to the conclusion that what needed to be done was just adding a secondary if-else to check through the inputs again and confirm the middle input. After adding this, the program works just fine with any of the numbers being in any of the inputs!

--- SortNumbers.py
+++ SortNumbers.py
@@ -1,6 +1,6 @@
 #Name: Raymond J. Spieldenner
 #Assignment: If Else - Project: Sorting Numbers
-#Date: 02/01/2022
+#Date: 02/02/2022
 
 #Input Variables:
 
@@ -8,26 +8,54 @@ userInput1 = int(input('Please input first number: '))
 userInput2 = int(input('Please input second number: '))
 userInput3 = int(input('Please input third number: '))
 
+#Sorting Variables:
+
+smallest = 0
+middle = 0
+largest = 0
+
 #Finding the largest number:
 if userInput1 > userInput2 and userInput1 > userInput3:
-    print(userInput1)
+    largest = userInput1
 elif userInput2 > userInput1 and userInput2 > userInput3:
-    print(userInput2)
+    largest = userInput2
 elif userInput3 > userInput1 and userInput3 > userInput2:
-    print(userInput3)
+    largest = userInput3
+
 #Finding the middle number:
+#if input one is more than the second but less then the third
 if userInput1 > userInput2 and userInput1 < userInput3:
-    print(userInput1)
+    middle = userInput1
+    #if 2 is more than 1 but less then 3
 elif userInput2 > userInput1 and userInput2 < userInput3:
-    print(userInput2)
+    middle = userInput2
+    #if 3 is more than 1 but less than 2
 elif userInput3 > userInput1 and userInput3 < userInput2:
-    print(userInput3)
+    middle = userInput3
 
 #finding the smallest number:
+    #1 is bottom
 if userInput1 < userInput2 and userInput1 < userInput3:
-    print(userInput1)
+    smallest = userInput1
+    #2 is bottom
 elif userInput2 < userInput1 and userInput2 < userInput3:
-    print(userInput2)
+    smallest = userInput2
+    #3 is the bottom
 elif userInput3 < userInput1 and userInput3 < userInput2:
-    print(userInput3)
-    
+    smallest = userInput3
+
+#This final block is to re-examine the third input so that if it's the lesser value, it still prints. That was
+#an issue I was having, where the median number wouldn't print if userInput3 was less than both of the other inputs.
+if userInput3 < userInput2 and userInput3 < userInput1:
+    smallest = userInput3
+if userInput2 < userInput1:
+    largest = userInput1
+    middle = userInput2
+else:
+    largest = userInput2
+    middle = userInput1
+#Print statements:
+
+print(smallest, "is the lesser value")
+print(middle, "is the median value")
+print(largest, "is the greater value")
